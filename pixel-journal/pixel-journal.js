182489d704547214f2098b250f670a500659f63f
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2016-02-20 21:47:55 +0000

    Joins the frontend with the backend

---
+++ pixel-journal/pixel-journal.js
@@ -0,0 +1,81 @@
+Notes = new Mongo.Collection('notes');
+
+if (Meteor.isClient) {
+    console.log("hello!");
+
+    Template.notes.helpers( {
+        'note': function() {
+            return Notes.find( {}, {sort: {createdAt: -1}} );
+        }
+    });
+
+    // Template.noteIndividual.helpers({
+    //     'checked': function() {
+    //         var isCompleted = this.completed;
+    //     }
+    // });
+
+    Template.quickAddNote.events({
+        // Add a quick note
+        'submit form': function(event) {
+            event.preventDefault();
+            var noteContent = $('[name="noteContent"]').val();
+            var noteName = noteContent;
+
+            if (noteContent.length > 10) {
+                noteName = noteContent.substring(0,10);
+            }
+
+            Notes.insert({
+                name: noteName,
+                content: noteContent,
+                createdAt: new Date()
+            })
+
+            $('[name="noteContent"]').val('');
+        }
+    });
+
+    Template.noteIndividual.events({
+        // Delete a quick note
+        'click .delete-note': function(event) {
+            event.preventDefault();
+            var documentId = this._id;
+            var confirm = window.confirm("Delete this task?");
+
+            if (confirm) {
+                Notes.remove( { _id: documentId } );
+            }
+        },
+
+        'keyup [name=noteIndividual]': function(event) {
+            var noteIndividual = $(event.target).val();
+            var documentId = this._id;
+
+            Notes.update({ _id: documentId }, {$set: { content: noteIndividual }} );
+        }
+    });
+}
+if (Meteor.isServer) {
+
+}
+
+// Routes
+Router.configure({
+    layoutTemplate: 'main'
+});
+Router.route('/', {
+    template: 'home',
+});
+Router.route('/register', {
+    name: 'register'
+});
+Router.route('/notebooks', {
+    name: 'notebooks'
+});
+Router.route('/sections', {
+    name: 'sections'
+});
+Router.route('/notes', {
+    name: 'notes'
+});
